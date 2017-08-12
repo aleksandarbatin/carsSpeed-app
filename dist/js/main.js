bddf74b91f919846c64277d2dffda80e8983a891
Alek21 <aleksandarbatin@gmail.com>
2017-08-13 01:17:00 +0200

    level_2 display data from json

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,35 @@
-//Static JSON call
+
+//JSON http REQUEST
 
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://api.myjson.com/bins/yzexp');
+ourRequest.open('GET', 'https://api.myjson.com/bins/vwuax');
 ourRequest.onload = function() {
-	var ourData = JSON.parse(ourRequest.responseText);
-	console.log(ourData);
+	if(ourRequest.status >= 200 && ourRequest.status < 400) {
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	} else {
+		console.log("We connected to the server, but it returned error.");
+	}
 }
+ourRequest.onerror = function() {
+	console.log("Connection error")
+};
 
 ourRequest.send();
+
+//RENDERING HTML
+
+function renderHTML(data) {
+	var carBox = document.getElementById("display");
+	var htmlString = "";
+	
+	for (var i = 0; i < data.cars.length; i++) {
+		htmlString += "<article class='displayBox'>" + "<div class='displayBox__inner'>" + "<img src='" + data.cars[i].image + "' />" + "<h2>" + data.cars[i].name + "</h2>" + "</div>" + "</article>";
+	
+	}
+
+	carBox.insertAdjacentHTML('beforeend', htmlString);
+}
+
+var x = document.getElementsByClassName('displayBox__inner');
+console.log(x.length);
