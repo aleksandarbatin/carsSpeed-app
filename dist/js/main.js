238d1ba437ba86d11ca5e5704e4fd41e4d7ab546
Alek21 <aleksandarbatin@gmail.com>
2017-08-13 18:50:55 +0200

    level_3 search-filter cars list

--- dist/js/main.js
+++ dist/js/main.js
@@ -33,3 +33,22 @@ function renderHTML(data) {
 
 var x = document.getElementsByClassName('displayBox__inner');
 console.log(x.length);
+
+// FILTER CARS ON INPUT SERACH
+
+function filterCars() {
+    var input, filter, filterDisplay, articleBox, carTitle, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    filterDisplay = document.getElementById("display");
+    articleBox = filterDisplay.getElementsByTagName("article");
+    for (i = 0; i < articleBox.length; i++) {
+        carTitle = articleBox[i].getElementsByTagName("h2")[0];
+        if (carTitle.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            articleBox[i].style.display = "";
+        } else {
+            articleBox[i].style.display = "none";
+
+        }
+    }
+}
